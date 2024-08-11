53c1c178155dd355c47841d6af35622b4300f228
DanteHalfeye <deadcoolest@gmail.com>
2024-08-11 15:44:36 -0500

    First commit

---
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class GameManager : SingletonMonobehaviour<GameManager>
+{
+    #region header DUNGEON LEVELS
+    [Space(10)]
+    [Header("DUNGEON LEVELS")]
+    #endregion DUNGEON LEVELS
+
+    #region Tooltip
+    [Tooltip("Populate with dungeon level scriptable objects")]
+    #endregion Tooltip
+
+    [SerializeField] private List<DungeonLevelSO> dungeonLevelList;
+
+    #region Tooltip
+    [Tooltip("Populate with the starting dungeon level for testing, first level = 0")]
+    #endregion Tooltip
+    [SerializeField] private int currentDungeonLevelListIndex = 0;
+
+    [HideInInspector] public GameState gameState;
+
+    private void Start()
+    {
+        gameState = GameState.gameStarted;
+    }
+
+    private void Update()
+    {
+        // Testing
+        if (Input.GetKeyDown(KeyCode.R))
+        HandleGameState();
+    }
+
+    /// <summary>
+    /// Handle game state
+    /// </summary>
+    private void HandleGameState()
+    {
+        // Handle game state
+        switch (gameState)
+        {
+            case GameState.gameStarted:
+                // Play first level
+                PlayDungeonLevel(currentDungeonLevelListIndex);
+
+                //gameState = GameState.playingLevel; 
+                break;
+
+        }
+    }
+
+    private void PlayDungeonLevel(int dungeonLevelListIndex)
+    {
+        // Build dungeon for level
+       bool dungeonBuiltSuccessfully = DungeonBuilder.Instance.GenerateDungeon(dungeonLevelList[dungeonLevelListIndex]);
+        if (!dungeonBuiltSuccessfully)
+        {
+            Debug.LogError("Couldn't build dungeon from specified rooms and node graphs");
+        }
+    }
+
+    #region Validation
+#if UNITY_EDITOR
+    private void OnValidate()
+    {
+        HelperUtilities.ValidateCheckEnumerableValues(this, nameof(dungeonLevelList), dungeonLevelList);
+    }
+#endif
+    #endregion Validation
+}
---
+++ Assets/Scripts/Utilities/HelperUtilities.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class HelperUtilities 
+{
+    ///<summary>
+    ///Empty String Debug Check
+    /// </summary>
+
+    public static bool ValidateCheckEmptySTring(Object thisObject, string fieldName, string stringToCheck)
+    {
+        if (stringToCheck == "")
+        {
+            Debug.Log(fieldName + " is empty and must contain a value in object " + thisObject.name.ToString());
+            return true;
+        }
+        return false;
+    }
+
+    ///<summary>
+    ///List empty or contains null value check - returns true if there is an error
+    /// </summary>
+     
+    public static bool ValidateCheckEnumerableValues(Object thisObject, string fieldName, IEnumerable enumerableObjectsToCheck) 
+    {
+        bool error = false;
+        int count = 0;
+
+        if (enumerableObjectsToCheck == null)
+        {
+            Debug.LogError(fieldName + " is null in object " + thisObject.name.ToString());
+            return true;
+        }
+
+        foreach (var item in enumerableObjectsToCheck)
+        {
+
+            if (item == null)
+            {
+                Debug.LogError(fieldName + " has null values in object " + thisObject.name.ToString());
+                error = true;
+
+            }
+            else
+            {
+                count++;
+            }
+        }
+        if (count == 0)
+        {
+            Debug.LogError(fieldName + " has no values in object " + thisObject.name.ToString());
+            error = true;
+        }
+        return error;
+    }
+}
