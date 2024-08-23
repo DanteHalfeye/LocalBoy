3ced795b7398151ed1f6a2d14fffca0abf23e71f
DanteHalfeye <deadcoolest@gmail.com>
2024-08-22 19:43:19 -0500

    .

---
+++ UNITY-LocalBoy/Assets/Scripts/Player/States/ExampleState.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExampleState : PlayerBaseState
+{
+    // A constructor of this class that passes the context of the base state class
+    public ExampleState(PlayerStateMachine currentContext) : base(currentContext)
+    {
+        
+    }
+
+    /// <summary>
+    /// Put here the logic it must follow like a void start
+    /// </summary>
+    public override void EnterState()
+    {
+        
+    }
+
+    /// <summary>
+    /// Reset whatever you did at Enterstate
+    /// </summary>
+    public override void ExitState()
+    {
+        
+    }
+
+    /// <summary>
+    /// Specify the conditions to leave the state, for this is useful you create bool variables in the statemachine and with a getter you pass it here
+    /// </summary>
+    public override void UpdateState()
+    {
+        
+    }
+
+    
+}
