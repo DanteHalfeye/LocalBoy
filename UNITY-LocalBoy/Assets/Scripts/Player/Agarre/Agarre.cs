0673ab578aa124d8169287fba02e66d37c914843
SebastianEsco <sebastian.escobarm@upb.edu.co>
2024-08-26 11:38:42 -0500

    basic enemy grab

--- UNITY-LocalBoy/Assets/Scripts/Player/Agarre/Agarre.cs
+++ UNITY-LocalBoy/Assets/Scripts/Player/Agarre/Agarre.cs
@@ -5,12 +5,33 @@ using UnityEngine;
 public class Agarre : MonoBehaviour
 {
     GameObject enemigo;
+    Rigidbody2D enemyRB;
+    CircleCollider2D enemiesInRange;
+    [SerializeField] float grabRange;
+    [SerializeField] LayerMask enemyLayer;
+    bool grabbing;
     public void Agarrar()
     {
-        //Pruebas, no funcionará así
-        enemigo = GameObject.Find("Enemigo");
-        enemigo.transform.position = gameObject.transform.position + new Vector3(0.5f ,0.5f,0f);
-        enemigo.transform.SetParent(gameObject.transform, false);
+        //Pruebas, no funcionará así del todo
+
+        Collider2D enemyToGrab = Physics2D.OverlapCircle(transform.position, grabRange, enemyLayer);
+
+        if (enemyToGrab != null && !grabbing)
+        {
+            enemigo = enemyToGrab.gameObject;
+
+            enemyRB = enemigo.GetComponent<Rigidbody2D>();
+            enemyRB.simulated = false;
+
+            
+
+            enemigo.transform.SetParent(gameObject.transform, false);
+            enemigo.transform.position =  new Vector3(0.5f, 0.5f, 0f) + gameObject.transform.localPosition;
+
+            grabbing = true; //Esto se cambiaría en la maquina de estados
+        }
+        
+        
 
     }
 
