3169e64b91b72234204d11325cba9314ffcf0ca8
ShadowjGames <98559091+ShadowjGames@users.noreply.github.com>
2024-08-28 11:22:01 -0500

    Estructura de UI

--- UNITY-LocalBoy/Assets/Scripts/UI/UIManager.cs
+++ UNITY-LocalBoy/Assets/Scripts/UI/UIManager.cs
@@ -6,48 +6,88 @@ using DG.Tweening;
 
 public class UIManager : MonoBehaviour
 {
-    [SerializeField] RectTransform mainMenu, gamePanel, optionsMenu, addsMenu, itemsMenu;
-    public float fadeTime = 1f;
+    [SerializeField] RectTransform mainMenu, gamePanel, optionsMenu, addsMenu, itemsMenu, pauseMenu, creditsPanel, gameOverPanel;
+    [SerializeField] Image darkOverlay;
+    [SerializeField] CanvasGroup gamePanelCanvasGroup;
+    public RectTransform rectTransform;
     public CanvasGroup canvaGroup;
-    public RectTransform rectTransform, colorTransform;
-    public Ease myEase;
     public List<GameObject> items = new List<GameObject>();
 
-    //public Image panelColor;
-    //public Color endColor;
+    [SerializeField] List<RectTransform> creditItems; 
+    public float fadeTime = 1f;
+    public float explosionDuration = 0.5f; 
+    public Vector2 explosionRange = new Vector2(100f, 100f); 
+    public Ease explosionEase = Ease.OutBack; 
+    public Ease fadeInEase = Ease.InQuad; 
+    public Ease myEase;
+
+    private enum MenuState { MainMenu, PauseMenu, None }
+    private MenuState previousState = MenuState.None;
+
     void Start()
     {
-        mainMenu.DOAnchorPos(new Vector2(0,0), 0.25f);
+        mainMenu.DOAnchorPos(new Vector2(0, 0), 0.25f);
+        optionsMenu.DOAnchorPos(new Vector2(0, 850), 0.25f);
+        darkOverlay.color = new Color(0, 0, 0, 0); 
     }
 
     public void optionsMenuButton()
     {
-        mainMenu.DOAnchorPos(new Vector2(-850, 0), 0.25f);
         optionsMenu.DOAnchorPos(new Vector2(0, 0), 0.25f);
     }
 
     public void closeOptionsMenu()
     {
         optionsMenu.DOAnchorPos(new Vector2(0, 850), 0.25f);
-        mainMenu.DOAnchorPos(new Vector2(0, 0), 0.25f);
     }
 
     public void gamePanelButton()
     {
-        mainMenu.DOAnchorPos(new Vector2(0,-1000), 0.25f);
+        mainMenu.DOAnchorPos(new Vector2(0, -1000), 0.25f);
         gamePanel.DOAnchorPos(new Vector2(0f, 0f), fadeTime, false).SetEase(myEase);
     }
 
     public void closeGamePanel()
     {
-        optionsMenu.DOAnchorPos(new Vector2(0, -1000), 0.25f);
-        gamePanel.DOAnchorPos(new Vector2(0, 0), 0.25f);
+        pauseMenu.DOAnchorPos(new Vector2(0, -850), 0f);
+        darkOverlay.DOFade(0f, 0.5f);
+        gamePanelCanvasGroup.interactable = true;
+        gamePanel.DOAnchorPos(new Vector2(0, -1000), 0.25f);
+        mainMenu.DOAnchorPos(new Vector2(0, 0), 0.25f);
     }
+
+    public void creditsPanelButton()
+    {
+        gamePanel.DOAnchorPos(new Vector2(0, -1000), 0f);
+        creditsPanel.DOAnchorPos(new Vector2(0, 0), 0.25f);
+        StartCoroutine("CreditItemsExplosionEffect");
+    }
+
+    public void closeCreditsPanel()
+    {
+        creditsPanel.DOAnchorPos(new Vector2(-1800, 0), 0.25f);
+
+        foreach (var item in creditItems)
+        {
+            // Establece la opacidad a 0
+            CanvasGroup itemCanvasGroup = item.GetComponent<CanvasGroup>();
+
+            if (itemCanvasGroup != null)
+            {
+                itemCanvasGroup.alpha = 0f;
+            }
+
+            // También puedes restablecer la escala si lo necesitas
+            item.localScale = Vector3.zero;
+        }
+
+        mainMenu.DOAnchorPos(new Vector2(0, 0), 0.25f);
+    }
+
 
     public void addsMenuButton()
     {
         addsMenu.DOAnchorPos(new Vector2(0, 0), 0.25f);
-        //colorTransform.DOColor(endColor, 0.5f).SetDelay(0.7f);
     }
 
     public void closeAddsMenu()
@@ -55,9 +95,32 @@ public class UIManager : MonoBehaviour
         addsMenu.DOAnchorPos(new Vector2(0, -850), 0.25f);
     }
 
+    public void pauseMenuButton()
+    {
+        pauseMenu.DOAnchorPos(new Vector2(0, 0), 0f);
+        darkOverlay.DOFade(0.5f, 0.5f);
+        gamePanelCanvasGroup.interactable = false; 
+    }
+
+    public void closePauseMenu()
+    {
+        pauseMenu.DOAnchorPos(new Vector2(0, -850), 0f);
+        darkOverlay.DOFade(0f, 0.5f);
+        gamePanelCanvasGroup.interactable = true; 
+    }
+
     public void itemsMenuButton()
     {
+        previousState = MenuState.MainMenu; 
         mainMenu.DOAnchorPos(new Vector2(850, 0), 0.25f);
+        itemsMenu.DOAnchorPos(new Vector2(0, 0), 0.25f);
+        panelFadeIn();
+    }
+
+    public void itemsMenuButtonInGame()
+    {
+        previousState = MenuState.PauseMenu; // Guardar estado anterior
+        pauseMenu.DOAnchorPos(new Vector2(850, 0), 0.25f);
         itemsMenu.DOAnchorPos(new Vector2(0, 0), 0.25f);
         panelFadeIn();
     }
@@ -65,8 +128,33 @@ public class UIManager : MonoBehaviour
     public void closeItemsMenu()
     {
         panelFadeOut();
-        itemsMenu.DOAnchorPos(new Vector2(850, 0), 0.25f);
-        mainMenu.DOAnchorPos(new Vector2(0, 0), 0.25f);
+
+        // Regresar al estado anterior
+        switch (previousState)
+        {
+            case MenuState.MainMenu:
+                itemsMenu.DOAnchorPos(new Vector2(850, 0), 0.25f);
+                mainMenu.DOAnchorPos(new Vector2(0, 0), 0.25f);
+                break;
+            case MenuState.PauseMenu:
+                itemsMenu.DOAnchorPos(new Vector2(850, 0), 0.25f);
+                pauseMenu.DOAnchorPos(new Vector2(0, 0), 0.25f);
+                break;
+        }
+
+        previousState = MenuState.None; // Resetea el estado anterior
+    }
+
+    public void gameOverButton()
+    {
+        gameOverPanel.DOAnchorPos(new Vector2(0, 0), 0.25f).SetEase(myEase);
+        gamePanel.DOAnchorPos(new Vector2(0, -1000), 0f);
+    }
+
+    public void closeGameOverPanel()
+    {
+        gameOverPanel.DOAnchorPos(new Vector2(0, 1000), 0f);
+        mainMenu.DOAnchorPos(new Vector2(0, 0), 0f);
     }
 
     public void panelFadeIn()
@@ -88,17 +176,47 @@ public class UIManager : MonoBehaviour
 
     IEnumerator itemsAnimation()
     {
-        foreach(var item in items)
+        foreach (var item in items)
         {
             item.transform.localScale = new Vector3(0f, 0f, 0f);
         }
 
-        foreach(var item in items)
+        foreach (var item in items)
         {
             item.transform.DOScale(1f, fadeTime).SetEase(Ease.OutBounce);
             yield return new WaitForSeconds(0.10f);
         }
     }
+
+    private IEnumerator CreditItemsExplosionEffect()
+    {
+        foreach (var item in creditItems)
+        {
+
+            // Mantener la posición original del elemento
+            Vector2 originalPos = item.anchoredPosition;
+
+            // Establecer la escala a cero y la opacidad a cero antes de la animación
+            item.localScale = Vector3.zero;
+            CanvasGroup itemCanvasGroup = item.GetComponent<CanvasGroup>();
+
+            if (itemCanvasGroup == null)
+            {
+                itemCanvasGroup = item.gameObject.AddComponent<CanvasGroup>();
+            }
+
+            // Animar la escala desde cero a uno para simular la explosión, sin mover la posición
+            item.DOScale(1f, explosionDuration).SetEase(explosionEase);
+
+            // Aplicar el efecto de fade in simultáneamente
+            itemCanvasGroup.DOFade(1f, explosionDuration).SetEase(Ease.Linear);
+
+            // Esperar antes de "explotar" el siguiente elemento
+            yield return new WaitForSeconds(0.3f);
+        }
+    }
+
+
 
     public void OnApplicationQuit()
     {
