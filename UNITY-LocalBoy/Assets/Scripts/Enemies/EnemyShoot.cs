0bac90b520c42d67abde1bedfd0a2e0705773da8
SebastianEsco <sebastian.escobarm@upb.edu.co>
2024-09-27 01:15:42 -0500

    Shoot preview, tipledisparo

--- UNITY-LocalBoy/Assets/Scripts/Enemies/EnemyShoot.cs
+++ UNITY-LocalBoy/Assets/Scripts/Enemies/EnemyShoot.cs
@@ -12,6 +12,39 @@ public class EnemyShoot : MonoBehaviour
     BulletPool bp;
     Shoot shoot;
 
+    GameObject[] balas = new GameObject[3];
+
+    string type;
+
+    // Define las opciones como un enum
+    public enum FeatureOption
+    {
+        disparoNormal,
+        disparoTriple,
+        escopeta
+    }
+
+    // Muestra el enum como un dropdown en el inspector
+    [SerializeField] private FeatureOption selectedOption;
+
+    void Update()
+    {
+        // Cambia el comportamiento según la opción seleccionada
+        switch (selectedOption)
+        {
+            case FeatureOption.disparoNormal:
+                type = "disparoNormal";
+                break;
+            case FeatureOption.disparoTriple:
+                type = "disparoTriple";
+                break;
+            case FeatureOption.escopeta:
+                type = "escopeta";
+                break;
+        }
+
+    }
+
     private void Awake()
     {
         bp = GameObject.FindGameObjectWithTag("BulletPool").GetComponent<BulletPool>();
@@ -26,18 +59,38 @@ public class EnemyShoot : MonoBehaviour
             if(timer > 0)
             {
                 timer -= Time.deltaTime;
+                if(timer * 100 /shootCooldown > 15 && timer * 100 / shootCooldown < 50) // entre el 50% y el 15% del tiempo de cooldown se muestra
+                {
+                    shoot.mostrandoRayo = true;
+                }
+                else
+                {
+                    shoot.mostrandoRayo = false;
+                }
             }
             else
             {
                 timer = shootCooldown;
-                Shoot();
+                Shoot(type);
             }
         }
     }
 
-    public void Shoot()
+    public void Shoot(string shootType)
     {
-        shoot.OnShoot(shoot.AutoShootDirection(), bp.RequerirBala());
+        if (type == "disparoNormal")
+        {
+            shoot.OnShoot(shoot.AutoShootDirection(), bp.RequerirBala());
+        }
+        else if(type == "disparoTriple")
+        {
+            for (int i = 0; i < balas.Length; i++)
+            {
+                balas[i] = bp.RequerirBala();
+            }
+            shoot.OnTripleShoot(shoot.AutoShootDirection(), balas);
+        }
+        
     }
 
 }
--- UNITY-LocalBoy/Assets/Scripts/Shoot/Shoot.cs
+++ UNITY-LocalBoy/Assets/Scripts/Shoot/Shoot.cs
@@ -5,9 +5,16 @@ using UnityEngine;
 public class Shoot : MonoBehaviour
 {
     public float fuerza;
-    [SerializeField] LayerMask targetLayer;
+    public string tagObjetivo;
+    [SerializeField] float desviacion;
 
     [SerializeField] UIManager uiManager;
+
+    public LineRenderer rayo; // El LineRenderer para el rayo
+    public float duracionRayo = 0.1f; // Duración del rayo antes de desaparecer
+    public float longitudRayo = 5f; // Longitud máxima del rayo
+
+    public bool mostrandoRayo;
 
     private void Awake()
     {
@@ -17,21 +24,49 @@ public class Shoot : MonoBehaviour
             uiManager.SetShoot(this);
         }
     }
-    // Te recomiendo que le pases el componente del rigidbody al metodo para que no lo llame cada disparo
+
+    private void FixedUpdate()
+    {
+        if (mostrandoRayo)
+        {
+            MostrarRayo(AutoShootDirection());
+        }
+        else
+        {
+            rayo.enabled = false;
+        }
+    }
+
+
     public void OnShoot(Vector2 direccion, GameObject bala) 
     {
         if (bala != null)
-        {
+        { 
             bala.transform.position = gameObject.transform.position;
             bala.GetComponent<Rigidbody2D>().velocity = direccion * fuerza;
         }
+    }
+
+
+    public void OnTripleShoot(Vector2 direccion, GameObject[] bala)
+    {
+        direccion.Normalize();
+        Vector2 perpendicular = new Vector2(-direccion.y, direccion.x);
+
+        // Disparo central
+        OnShoot(direccion, bala[0]);
+
+        // Disparo con desviación hacia un lado
+        OnShoot(direccion + (perpendicular * desviacion), bala[1]);
+        // Disparo con desviación hacia el otro lado
+        OnShoot(direccion - (perpendicular * desviacion), bala[2]);
     }
 
     public Vector2 AutoShootDirection()
     {
         GameObject jugador;
 
-        if (GameObject.FindGameObjectWithTag("Player")) //usa un find with tag, gasta menos recursos (Ya le puse el tag)
+        if (GameObject.FindGameObjectWithTag("Player")) 
         {
              jugador = GameObject.Find("Player");
         }
@@ -51,4 +86,39 @@ public class Shoot : MonoBehaviour
             return new Vector2(1, 1); // No necesitas un else, ya que si no hay jugador no deberian spawnear enemigos
         }
     }
+
+
+
+    private void MostrarRayo(Vector2 direccion)
+    {
+        // Raycast para detectar colisiones
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, direccion, longitudRayo);
+
+        // Activar el LineRenderer
+        rayo.enabled = true;
+
+        // Establecer la posición de inicio del rayo
+        rayo.SetPosition(0, transform.position); // Punto inicial en la posición del objeto que dispara
+
+        // Si el Raycast detecta una colisión
+        if (hit.collider != null)
+        {
+            // Verifica si el objeto tiene el tag deseado
+            if (hit.collider.CompareTag(tagObjetivo))
+            {
+                Debug.Log("Colisión con el objeto: " + hit.collider.gameObject.name);
+            }
+
+            // Establecer el punto final del rayo en el punto de colisión
+            rayo.SetPosition(1, hit.point);
+        }
+        else
+        {
+            rayo.SetPosition(1, (Vector2)transform.position + (direccion * longitudRayo));
+        }
+    }
+
+
+
+
 }
